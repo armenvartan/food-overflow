101e239beb486be62ee9b9aab829cb00d57420b1
Armen Vartan <vartan.armen@gmail.com>
2014-08-11 15:02:18 -0400

    changed method name in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
 	has_secure_password
 
-	has_many :questions
+	has_many :questions # delete these two
 	has_many :comments, through: :questions
 
 	before_validation :check_password
 
-	def check_password
+	def workaround_to_make_has_secure_password_work
 		unless self.access_token.nil?
 			self.password = "gh-#{access_token}"
 			self.password_confirmation = self.password
